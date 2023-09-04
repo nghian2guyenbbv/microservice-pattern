3f7f16a59a6230b69c0c6d5412ea7479af75ef42
nghianguyenbbv <nghia2nguyen@bbv.vn>
2023-09-04 19:14:36 +0700

    add 4 service

--- kafka/spring-emailService/src/main/java/com/kafka/emailservice/SpringEmailServiceApplication.java
+++ kafka/spring-emailService/src/main/java/com/kafka/emailservice/SpringEmailServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringEmailServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEmailServiceApplication.class, args);
 	}
